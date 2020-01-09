c3b0e82ea7e77792710e38653795095fc94ba6a7
Marije-van-der-Werf <53301264+Marije-van-der-Werf@users.noreply.github.com>
2020-01-09 14:59:24 +0100

    data_inlezen uitgebreid
    Datums omgezet naar datumtype
    Sales data gefilterd op vanaf 1-1-2017
    Verschillende kolommen afgerond
    Gefilterd op alleen mobiele telefonie

--- data_inlezen.R
+++ data_inlezen.R
@@ -6,25 +6,47 @@
 library(dplyr)
 library(magrittr)
 
-Competitor <- read.csv("Competitor Data - TV.csv", sep = ",", header = TRUE)
+Competitor <- read.csv("Competitor Data - TV.csv", sep = ",", header = TRUE) %>% 
+    mutate(date = as.Date(date))
 Display <- read.csv("Display Data.csv", sep = ";", header = TRUE) %>% 
-    mutate(Date = substr(Date, 1, 10))
+    mutate(Date = as.Date(substr(Date, 1, 10)))
 Display <- rename(Display, date = Date, cost = Cost)
 OtherMarketing <- read.csv("Other_Marketing_Data.csv", sep = ";", header = TRUE) %>% 
-    mutate(date = substr(date, 1, 10))
+    mutate(date = as.Date(substr(date, 1, 10)))
 Sales <- read.csv("Sales Data.csv", sep = ",", header = TRUE) %>% 
-    mutate(date = substr(invoice_date, 1, 10)) %>% 
+    mutate(date = as.Date(substr(invoice_date, 1, 10))) %>% 
+    filter(date >= "2017-01-01") %>% 
     select(date, everything(), -invoice_date)
-Social <- read.csv("Social Data.csv", sep = ",", header = TRUE)
+Social <- read.csv("Social Data.csv", sep = ",", header = TRUE) %>% 
+    mutate(date = as.Date(date))
 
 #' Data opschonen
-OtherMarketing2 <- OtherMarketing %>% 
-    mutate(tv_cost = as.numeric(as.character(tv_cost)),
-           tv_cost = round(tv_cost, 0),
-           radio_cost = as.numeric(as.character(radio_cost)),
-           radio_cost = round(radio_cost, 0))
-
-
-
+#' Afronden
+Display <- Display %>% 
+    mutate(cost = round(cost, 2))
+Social <- Social %>% 
+    mutate(cost = round(cost, 2))
+Sales <- Sales %>% 
+    mutate(products_sold = round(products_sold, 0),
+           distinct_orders = round(distinct_orders, 0))
 OtherMarketing <- OtherMarketing %>% 
-    mutate_at(vars(tv_cost, radio_cost, ooh_cost, search_volume, estimated_daily_sales_value), funs(as.numeric()))
+    mutate(delivered_emails = round(delivered_emails, 0),
+           opened_emails = round(opened_emails, 0),
+           clicks_emails = round(clicks_emails, 0),
+           estimated_daily_sales_value = round(estimated_daily_sales_value, 0),
+           searchads_cost = round(searchads_cost, 2),
+           shoppingads_cost = round(shoppingads_cost, 2),
+           tv_cost = round(tv_cost, 2),
+           radio_cost = round(radio_cost, 2),
+           ooh_cost = round(ooh_cost, 2))
+
+#' Filter op Mobiele telefoons (product_type_id == 2093)
+Display2093 <- Display %>% 
+    filter(product_type_id == 2093)
+OtherMarketing2093 <- OtherMarketing %>% 
+    filter(key %in% c("2093_11", "2093_31"))
+Sales2093 <- Sales %>% 
+    filter(product_type_id == 2093)
+Social2093 <- Social %>% 
+    filter(product_type_id == 2093)
+
